13eda8686005c31b4bf99b74ed227e820de80364
nshkuratova <nika.shkuratova@gmail.com>
2015-11-06 22:07:29 +0300

    Переход на БД h2
    Перевела на БД h2 все.
    Перед деплоем теперь нужно запустить консоль БД
    (http://www.h2database.com/html/main.html), указать в настройках URL =
    jdbc:h2:tcp://localhost/~/kuriculo и подключиться. И потом уже деплоить.
    По умолчанию при первом деплое должны создаваться 3 пользователя и 9
    новостей. Таблицы и данные в БД сохраняются и не перезатираются во время
    нового деплоя.

--- src/main/java/net/kuriculo/dao/DataBaseInitializer.java
+++ src/main/java/net/kuriculo/dao/DataBaseInitializer.java
@@ -35,22 +35,29 @@ public class DataBaseInitializer
 
 	public void initDataBase()
 	{
-		User userUser = new User("user", this.passwordEncoder.encode("user"));
-		userUser.addRole("user");
-		this.userDao.save(userUser);
+		if(userDao.findByName("admin")== null) {
+			User userUser = new User("user", this.passwordEncoder.encode("user"));
+			userUser.addRole("user");
+			this.userDao.save(userUser);
 
-		User adminUser = new User("admin", this.passwordEncoder.encode("admin"));
-		adminUser.addRole("user");
-		adminUser.addRole("admin");
-		this.userDao.save(adminUser);
+			User adminUser = new User("admin", this.passwordEncoder.encode("admin"));
+			adminUser.addRole("user");
+			adminUser.addRole("admin");
+			this.userDao.save(adminUser);
 
-		long timestamp = System.currentTimeMillis() - 1000 * 60 * 60 * 24;
-		for (int i = 0; i < 10; i++) {
-			NewsEntry newsEntry = new NewsEntry();
-			newsEntry.setContent("This is example content " + i);
-			newsEntry.setDate(new Date(timestamp));
-			this.newsEntryDao.save(newsEntry);
-			timestamp += 1000 * 60 * 60;
+			//TODO remove later
+			User nika = new User("nika", this.passwordEncoder.encode("nika"));
+			nika.addRole("nika");
+			this.userDao.save(nika);
+
+			long timestamp = System.currentTimeMillis() - 1000 * 60 * 60 * 24;
+			for (int i = 0; i < 10; i++) {
+				NewsEntry newsEntry = new NewsEntry();
+				newsEntry.setContent("This is example content " + i);
+				newsEntry.setDate(new Date(timestamp));
+				this.newsEntryDao.save(newsEntry);
+				timestamp += 1000 * 60 * 60;
+			}
 		}
 	}
 
